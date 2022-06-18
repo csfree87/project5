cc185bf59e3bb60977d03df4190fbd0131e8ca42
csfree87 <csfree@icloud.com>
2022-06-18 04:13:16 +0000

    Added project status explanation in Readme and image of site so far, troubles with heroku deployment

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Regretfully this current submission is lacking many things needed to complete th
 I've created an e-commerce site focused on selling fragrances, soaps, lotions, and candles and intend to add more options to my completed project. 
 
 This readme file will also be made to standard upon resubmission. I understand there may not be much to provide feedback on, but appreciate any offered and look forward to presenting a much more polished and completed website. 
+
+![This is an image](/media/Screenshot%202022-06-18%20at%2006.09.10.png)
--- laTerre/settings.py
+++ laTerre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f8orcywrwf8=b3xk17b8iimmps)2uss=5xr&t909=lqf@kyz#w'
+SECRET_KEY = 'randomkey87'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['laterre.herokuapp.com', 'localhost']
 
 
 # Application definition
